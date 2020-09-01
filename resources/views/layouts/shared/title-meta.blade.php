266d3fdd02f50b3b35c89616b0c95f4ba0d82c5e
bsjang <bsjang@bs-soft.co.kr>
2020-09-01 10:18:37 +0900

    first commit

---
+++ resources/views/layouts/shared/title-meta.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8" />
+<title>{{$title ?? ''}} | Scare - 차세대 감성관리 솔루션</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+<meta content="Coderthemes" name="author" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<!-- App favicon -->
+<link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
